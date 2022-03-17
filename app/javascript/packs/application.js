1ec5cb94b7cc58e219d455bf1bde36fa2ffb0b7d
Tyler Freedman <mail@tylerfreedman.com>
2022-03-17 10:53:49 -0400

    Removed more dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
 import "@fortawesome/fontawesome-free/css/all"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-ActiveStorage.start()
 
 window.$ = window.jQuery = require('jquery');
 const fancybox = require('@fancyapps/fancybox');
